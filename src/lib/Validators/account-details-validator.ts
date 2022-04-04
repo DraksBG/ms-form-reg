caaa538fa126030fdfeae8efd4606a9c64ad1024
DraksBG <52380770+DraksBG@users.noreply.github.com>
2022-04-04 09:56:39 +0300

    Added validation for day month year inputs

--- src/lib/Validators/account-details-validator.ts
+++ src/lib/Validators/account-details-validator.ts
@@ -2,11 +2,23 @@ import {
   inputValuesErrors,
   IValuesValidate,
 } from "../../components/AccountDetails/AccountDetails.type";
-import { VALID_EMAIL_REG, VALID_PASSWORD_REG } from "./regex";
+import { userDetailsValuesErrors } from "../../components/UserDetails/UserDetails.type";
+import {
+  VALID_DAY,
+  VALID_EMAIL_REG,
+  VALID_FIRST_NAMES,
+  VALID_MONTH,
+  VALID_PASSWORD_REG,
+  VALID_YEAR,
+} from "./regex";
 
 export const fieldsValidator = (value: string, name: string) => {
   const isEmailValid = new RegExp(VALID_EMAIL_REG);
   const isPasswordValid = new RegExp(VALID_PASSWORD_REG);
+  const isNameValid = new RegExp(VALID_FIRST_NAMES);
+  const isDayValid = new RegExp(VALID_DAY);
+  const isMonthValid = new RegExp(VALID_MONTH);
+  const isYearValid = new RegExp(VALID_YEAR);
 
   switch (name) {
     case "email":
@@ -46,6 +58,64 @@ export const fieldsValidator = (value: string, name: string) => {
         valid: value !== "",
         errorMessage: `${
           value === "" ? inputValuesErrors.emptySecurityQuestionB : ""
+        }`,
+      };
+
+    case "firstName":
+      return {
+        valid: isNameValid.test(value),
+        errorMessage: `${
+          !isNameValid.test(value) && value !== ""
+            ? userDetailsValuesErrors.firstName
+            : value === ""
+            ? userDetailsValuesErrors.emptyFirstName
+            : ""
+        }`,
+      };
+
+    case "lastName":
+      return {
+        valid: isNameValid.test(value),
+        errorMessage: `${
+          !isNameValid.test(value) && value !== ""
+            ? userDetailsValuesErrors.lastName
+            : value === ""
+            ? userDetailsValuesErrors.emptyLastName
+            : ""
+        }`,
+      };
+
+    case "day":
+      return {
+        valid: isDayValid.test(value),
+        errorMessage: `${
+          !isDayValid.test(value) && value !== ""
+            ? userDetailsValuesErrors.day
+            : value === ""
+            ? userDetailsValuesErrors.emptyDay
+            : ""
+        }`,
+      };
+    case "month":
+      return {
+        valid: isMonthValid.test(value),
+        errorMessage: `${
+          !isMonthValid.test(value) && value !== ""
+            ? userDetailsValuesErrors.month
+            : value === ""
+            ? userDetailsValuesErrors.emptyMonth
+            : ""
+        }`,
+      };
+    case "year":
+      return {
+        valid: isYearValid.test(value),
+        errorMessage: `${
+          !isYearValid.test(value) && value !== ""
+            ? userDetailsValuesErrors.year
+            : value === ""
+            ? userDetailsValuesErrors.emptyYear
+            : ""
         }`,
       };
 
--- src/lib/Validators/regex.ts
+++ src/lib/Validators/regex.ts
@@ -1,3 +1,9 @@
 export const VALID_EMAIL_REG =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const VALID_PASSWORD_REG = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,12}$/i;
+
+export const VALID_FIRST_NAMES = /^([a-zA-Z]+?)([-\s'][a-zA-Z]+)*?$/i;
+export const VALID_DAY =
+  /(?:\b|\/|-)((?:[1-9]|0[1-9]|[1-2][0-9]|3[0-1])(?:\b|\/|-))/i;
+export const VALID_MONTH = /^(0?[1-9]|1[012])$/i;
+export const VALID_YEAR = /^(?:19|20)\d{2}/i;
